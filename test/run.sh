164adaae498a672b9dcbbf7c39c9535f0a9fc1b6
zoechi <gzoechi@gmail.com>
2014-05-18 20:29:40 +0200

    #47

---
+++ test/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+which content_shell
+if [[ $? -ne 0 ]]; then
+  $DART_SDK/../chromium/download_contentshell.sh
+  unzip content_shell-linux-x64-release.zip
+
+  cs_path=$(ls -d drt-*)
+  PATH=$cs_path:$PATH
+fi
+
+content_shell --dump-render-tree core_ajax.html
+content_shell --dump-render-tree core_collapse.html
+#content_shell --dump-render-tree polymer_file.html
+#content_shell --dump-render-tree polymer_localstorage.html
+#content_shell --dump-render-tree polymer_selection.html
+#content_shell --dump-render-tree polymer_selection_multi.html
+#content_shell --dump-render-tree polymer_selector_activate_event.html
+#content_shell --dump-render-tree polymer_selector_basic.html
+#content_shell --dump-render-tree polymer_selector_multi.html
