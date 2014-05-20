9f72e702e8d95695e53ab88ee4043bb3152a1959
zoechi <gzoechi@gmail.com>
2014-05-20 22:03:19 +0200

    #19 add to entry points for docgen generation

--- lib/core_elements.dart
+++ lib/core_elements.dart
@@ -3,4 +3,5 @@ library core_elements;
 export 'package:core_elements/core_ajax/core_ajax.dart';
 export 'package:core_elements/core_ajax/core_xhr.dart';
 export 'package:core_elements/core_collapse/core_collapse.dart';
+export 'package:core_elements/core_layout_grid/core_layout_grid.dart';
 export 'package:core_elements/core_tooltip/core_tooltip.dart';
